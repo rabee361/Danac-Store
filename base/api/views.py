df2df25b15d5bbab4576a568c7fd656e35eace06
rabee361 <rha60540@gmail.com>
2023-12-11 13:09:06 +0300

    changes

---
+++ base/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from base.models import *
+from .serializers import *
+from rest_framework.generics import ListAPIView, RetrieveAPIView , CreateAPIView
+
+
