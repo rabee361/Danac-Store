fdccb6f5fc8f8304a41df8ca6cd518bea0127c84
rabee361 <rha60540@gmail.com>
2023-12-20 07:10:51 +0300

    changes

--- base/api/views.py
+++ base/api/views.py
@@ -111,6 +111,20 @@ class RetUpdDesProduct(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
 ############################ CART HNADLING ######################
         
 class CartProductsView(ListAPIView):
@@ -747,7 +761,6 @@ class RetUpdDesReturnGoodClient(RetrieveUpdateDestroyAPIView):
     queryset = ReturnedGoodsClient.objects.all()
     serializer_class = ReturnedGoodsClientSerializer
     # permission_classes = [permissions.IsAuthenticated]
-
 
 
 # ------------------------------------------DAMAGED PRODUCTS------------------------------------------
