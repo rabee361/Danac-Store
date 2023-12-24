778e2317448e1fec8ca1778d2b92f4d0395e7d69
rabee361 <rha60540@gmail.com>
2023-12-24 20:22:30 +0300

    changes

--- systempro/settings.py
+++ systempro/settings.py
@@ -207,5 +207,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Replace with yo
 EMAIL_HOST = 'smtp.gmail.com' # Replace with your email host for gmail -> 'smtp.gmail.com'
 EMAIL_PORT = 587  # Replace with your email port
 EMAIL_USE_TLS = True  # Set to False if your email server doesn't use TLS
-EMAIL_HOST_USER = 'jacoubakizi@gmail.com'  # Replace with your email username
-EMAIL_HOST_PASSWORD = 'rdedzduyncjjyhgs'     
+EMAIL_HOST_USER = 'jacoubakizi81@gmail.com'  # Replace with your email username
+EMAIL_HOST_PASSWORD = 'pghstcxtcrcrjtmu'     
