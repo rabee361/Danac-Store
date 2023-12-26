b9d49d77f2a42abe7b5452937aa5946a71965477
rabee361 <rha60540@gmail.com>
2023-12-26 13:40:46 +0300

    changes

--- base/admin.py
+++ base/admin.py
@@ -51,7 +51,7 @@ class AdminCustomUser(UserAdmin, admin.ModelAdmin):
                 {'fields':('phonenumber','email', 'password',)}
             ),
             ('User Information',
-                {'fields':('username', 'first_name', 'last_name','image')}
+                {'fields':('username', 'first_name', 'last_name','image','location')}
             ),
             ('Permissions', 
                 {'fields':('is_verified', 'is_accepted', 'is_staff', 'is_superuser', 'is_active', 'groups','user_permissions', 'user_type')}
