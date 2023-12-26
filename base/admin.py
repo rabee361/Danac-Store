d5a8547f22189343aa8713fdad8cae9194b14d34
rabee361 <rha60540@gmail.com>
2023-12-26 12:25:22 +0300

    changes

--- base/admin.py
+++ base/admin.py
@@ -3,44 +3,58 @@ from .models import *
 from leaflet.admin import LeafletGeoAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth.admin import UserAdmin
+import random
+from api.utils import Utlil
+from base.api.serializers import CodeVerivecationSerializer
 
-
-class AdminCustomUser(UserAdmin, LeafletGeoAdmin):
+class AdminCustomUser(UserAdmin, admin.ModelAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    actions = ['accept_user']
-    
-    # def accept_user(self, request, queryset):
-    #     queryset.update(is_active=True)
-    #     user = queryset.get(is_active=True)
-    #     rand_num = random.randint(1,10000)
-    #     email_body = 'Hi '+user.username+' Use the code below to verify your email \n'+ str(rand_num)
-    #     data = {'email_body':email_body, 'to_email':user.email, 'email_subject':'Verify your email'}
-    #     Utlil.send_eamil(data)
-    #     client = Client.objects.create(
-    #         name=user.username,
-    #         address = user.address,
-    #         phonenumber = user.phonenumber
-    #     )
-    #     code_verivecation = random.randint(1000,9999)
-    #     serializer = CodeVerivecationSerializer(data ={
-    #             'user':user.id,
-    #             'code':code_verivecation
-    #         })
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     cart = Cart.objects.create(customer=client)
-    #     cart.save()
+    list_filter = ['is_accepted']
+    actions = ['Accept_User', 'Refusal_User']
+    list_display = ['id', 'email', 'is_staff', 'is_accepted']        
+
+    def Accept_User(self, request, queryset):
+        queryset.update(is_active=True)
+        user_type = UserType.objects.get(user_type='عميل')
+        queryset.update(is_accepted=True, user_type=user_type)
+        user = queryset.get(is_active=True)
+        rand_num = random.randint(1,10000)
+        client = Client.objects.create(
+            name=user.username,
+            address = user.address,
+            phonenumber = user.phonenumber
+        )
+        code_verivecation = random.randint(1000,9999)
+        email_body = 'Hi '+user.username+' Use the code below to verify your email \n'+ str(code_verivecation)
+        data = {'email_body':email_body, 'to_email':user.email, 'email_subject':'Verify your email'}
+        Utlil.send_eamil(data)
+        serializer = CodeVerivecationSerializer(data ={
+                'user':user.id,
+                'code':code_verivecation
+            })
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        cart = Cart.objects.create(customer=client)
+        cart.save()
+
+
+    def Refusal_User(self, request, queryset):
+        user = queryset.get(is_accepted=False)
+        email_body = 'Hi '+user.username+' نعتذر منك لقد تم رفض حسابك لأن موقعك بعيد ولا يمكن توصيل طلباتك \n'
+        data = {'email_body':email_body, 'to_email':user.email, 'email_subject':'Refusal Account'}
+        Utlil.send_eamil(data)
+        user.delete()
 
     fieldsets = (
         (None, 
                 {'fields':('phonenumber','email', 'password',)}
             ),
             ('User Information',
-                {'fields':('username', 'first_name', 'last_name','location','image')}
+                {'fields':('username', 'first_name', 'last_name','image')}
             ),
             ('Permissions', 
-                {'fields':('is_verified', 'is_staff', 'is_superuser', 'is_active', 'groups','user_permissions', 'user_type')}
+                {'fields':('is_verified', 'is_accepted', 'is_staff', 'is_superuser', 'is_active', 'groups','user_permissions', 'user_type')}
             ),
             ('Registration', 
                 {'fields':('date_joined', 'last_login',)}
@@ -50,12 +64,12 @@ class AdminCustomUser(UserAdmin, LeafletGeoAdmin):
     add_fieldsets = (
         (None, {'classes':('wide',),
             'fields':(
-                'phonenumber','email','username', 'password1', 'password2'
+                'phonenumber','email' , 'username', 'password1', 'password2', 'user_type'
             ),}
             ),
     )
 
-    # # accept_user.short_descreption = 'Accept User For complet registration'
+    # accept_user.short_descreption = 'Accept User For complet registration'
     # accept_user.short_descreption = 'print_user'
 
 @admin.register(Client)
