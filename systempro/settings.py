98bb027aec712a6241c92f2705c32bbf407a69ab
rabee361 <rha60540@gmail.com>
2024-01-10 14:47:10 +0300

    changes

--- systempro/settings.py
+++ systempro/settings.py
@@ -88,6 +88,7 @@ AUTH_USER_MODEL = 'base.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
