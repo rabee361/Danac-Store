795c0c7abe796b4ac9ed78c940fffd49ac41726c
rabee361 <rha60540@gmail.com>
2024-03-16 14:10:44 +0300

    chanegs

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -2265,10 +2265,12 @@ class OutputSerializer2(serializers.ModelSerializer):
     def create(self, validated_data):
         request = self.context.get('request')
         order_id = validated_data.pop('order_id')
-        client_data = validated_data.pop('client', None)
+        # client_data = validated_data.pop('client', None)
+        order = Order.objects.get(id=order_id)
+        client_data = order.client
         remaining_amount = validated_data.pop('remaining_amount', 0.0)
         employee = Employee.objects.filter(phonenumber=request.user.phonenumber).first()
-        client = Client.objects.get(id=client_data.id)
+        client = Client.objects.get(id=client_data)#### client_data.id
         cart = Cart.objects.get(customer=client)
         instance = Output.objects.create(employee=employee, client=client,barcode=cart.barcode, **validated_data)
         client.debts += remaining_amount
